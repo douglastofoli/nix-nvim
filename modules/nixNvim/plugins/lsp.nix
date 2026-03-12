956932e84a3d75dc187ef70e01ec059c20731695
douglastofoli <tofoli.douglas@hotmail.com>
2026-03-11 23:20:03 -0300

    fix: use native lsp

--- modules/nixNvim/plugins/lsp.nix
+++ modules/nixNvim/plugins/lsp.nix
@@ -18,10 +18,18 @@
     ];
 
     extraLua = ''
-      local lspconfig = require("lspconfig")
-
-      lspconfig.lua_ls.setup({})
-      lspconfig.nil_ls.setup({})
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+      })
+      vim.lsp.config("nil_ls", {
+        cmd = { "nil" },
+        filetypes = { "nix" },
+        root_markers = { "flake.nix", ".git" },
+      })
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("nil_ls")
     '';
   };
 }
